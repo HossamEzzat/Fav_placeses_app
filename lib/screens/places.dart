3374a7975b16d651c3ae821abb223c94779b38a8
User <hossamezzat199@gmail.com>
2025-11-20 08:49:31 +0200

    place_detail

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -1,14 +1,18 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../providers/user_places.dart';
 import '../widgets/places_list.dart';
 import 'add_place.dart';
 
-class PlacesScreen extends StatelessWidget {
+class PlacesScreen extends ConsumerWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userPlaces = ref.watch(userPlacesProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Places'),
@@ -26,7 +30,7 @@ class PlacesScreen extends StatelessWidget {
         ],
       ),
       body: PlacesList(
-        places: [],
+        places: userPlaces,
       ),
     );
   }
